5437ee47a9690492ba33bae8cb57f58ef843a76a
Radhia Hamed <radhiahamed187@gmail.com>
2025-01-21 01:16:31 +0100

    angular project

---
+++ helloworld/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  
+  title = "Angular";
+  condition=true;
+  number=123;
+ source="assets/angular.png";
+
+nameList=['Ahmed','Radhia','Imen'];
+
+
+}
